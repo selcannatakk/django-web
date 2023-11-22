3f622388abd5e76cc913737b6e85fecba8296bba
selcanatak2629 <192803025@ogr.cbu.edu.tr>
2023-11-22 22:39:26 +0300

    refactor

---
+++ angular-django-postgresql/Readme.md
@@ -0,0 +1,1 @@
+Django - PostgreSQL - Angular
---
+++ django-graphql-sqllite3/backend/books/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from graphene_django.views import GraphQLView
+from django.views.decorators.csrf import csrf_exempt
+from books.schema import schema
+
+urlpatterns = [
+    path("graphql", csrf_exempt(GraphQLView.as_view(graphql=True, schema=schema))),
+]
